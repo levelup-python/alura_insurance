93897a73e85afa1c6d7fe9c5bade3182e200161d
Domingas Mendes Duarte <dduarte@mag.com.br>
2023-03-27 14:05:41 -0300

    Criando modelos para representar Segurado e Beneficiario

--- PyOrientacaoObjetos/alura_insurance/Contato.py
+++ PyOrientacaoObjetos/alura_insurance/Contato.py
@@ -4,7 +4,3 @@ class Contato:
         self.telefone_residencial = telefone_residencial
         self.telefone_comercial = telefone_comercial
         self.email = email
-
-
-dados = Contato('21 99985 3145', '21 3222 6666',
-                '21 0000 9999', 'ana@hotmail.com')
