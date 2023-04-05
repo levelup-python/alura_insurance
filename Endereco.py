29c6d291a24ee4eabb199231fc5e003f06fe8ffc
dguerrieri <dguerrieri@mag.com.br>
2023-04-05 20:29:55 -0300

    abrindo os arquivos

---
+++ Endereco.py
@@ -0,0 +1,11 @@
+class Endereco():
+    def __init__(self, rua, numero, complemento, cep, estado, cidade):
+        self._rua = rua
+        self._numero = numero
+        self._complemento = complemento
+        self._cep = cep
+        self._estado = estado
+        self._cidade = cidade
+    
+    def __str__(self):
+        return f"{self._rua} - {self._numero}"
