84df3b3020ac13512c25deaccf771b1b2ab71a82
Domingas Mendes Duarte <dduarte@mag.com.br>
2023-04-17 08:46:02 -0300

    criando ambiente virtual e diretorios pro pytest

--- PyOrientacaoObjetos/alura_insurance/tests/tests_alura_insurance.py
+++
@@ -1,7 +0,0 @@
-from Cliente import Cliente
-
-
-class Teste:
-
-    def teste_retornar_nome_Completo(self):
-        pass
---
+++ PyOrientacaoObjetos/alura_insurance/tests/tests_class_cliente.py
@@ -0,0 +1,8 @@
+from Cliente import Cliente
+
+
+class Teste:
+
+    def teste_quando_chama_nomecompleto_retornar_nome_sobrenome(self):
+
+        pass
