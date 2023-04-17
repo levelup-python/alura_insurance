675978f5a9f5b49d657e7d5c679cb5c2d6568455
Domingas Mendes Duarte <dduarte@mag.com.br>
2023-04-17 17:16:03 -0300

    primeiro pytest

--- PyOrientacaoObjetos/alura_insurance/tests/test_class_cliente.py
+++ PyOrientacaoObjetos/alura_insurance/tests/test_class_cliente.py
@@ -4,13 +4,14 @@ from Cliente import Cliente
 class Teste:
 
     def test_quando_chama_nomecompleto_retornar_nome_sobrenome(self):
+        # Given contexto
         nome = 'Ana'
         sobrenome = 'Gonçalves'
         esperado = 'Ana Gonçalves'
 
         cliente1 = Cliente(nome, sobrenome, '123.123.123-12',
-                           '242424', '2000/10/13')
+                           '242424', '2000-10-13')
 
-        resultado = cliente1.calcular_nome_completo()
+        resultado = cliente1.calcular_nome_completo()  # When - Ação
 
         assert resultado == esperado  # Then - Desfecho
