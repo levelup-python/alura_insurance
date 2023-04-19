cfbdc9692136e3c7f0e9a353cb67c96070b774ed
Domingas Mendes Duarte <dduarte@mag.com.br>
2023-04-19 11:19:43 -0300

    criando test para testar a função que calcula comissao total

--- PyOrientacaoObjetos/alura_insurance/tests/test_class_comissao.py
+++ PyOrientacaoObjetos/alura_insurance/tests/test_class_comissao.py
@@ -1,23 +1,23 @@
 from Apolice import Apolice
-from Segurado import Segurado
+from Corretor import Corretor
 
 
 class Teste:
 
-    def test_valor_premio_total_retorna_soma_dos_premios_da_apolice1_com_apolice2(self):
+    def test_comissao_total_retorna_soma_das_comissoes_da_apolice1_com_apolice2(self):
         # Given contexto
         premio1 = 5000
         premio2 = 5000
-        esperado = 10000
+        esperado = 550
 
         apolice1 = Apolice('238575', premio1, 'segurado1', '1980-02-01',
                            '22/04/1999', 'VIDA', 'ativo')
         apolice2 = Apolice('000000', premio2, 'segurado1', '1980-02-01',
                            '22/04/1999', 'VIAGEM', 'ativo')
 
-        cliente1 = Segurado('Ana', 'endereco', 'contato',
-                            'benef', [apolice1, apolice2], 'Corretor')
+        corretor1 = Corretor('Antonio', 'goncalves', '234567', [
+            apolice1, apolice2], 'contato_corretor')
 
-        resultado = cliente1.premio_total  # When - Ação
+        resultado = corretor1.comissao_total  # When - Ação
 
         assert resultado == esperado  # Then - Desfecho
