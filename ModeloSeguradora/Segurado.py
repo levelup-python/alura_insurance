7b6ce66a436fbd8beb81871cff13cc58a41c24a8
jessassumpcao <you@example.com>
2023-04-23 17:50:15 -0300

    Inserindo informações pessoais do segurado

--- ModeloSeguradora/Segurado.py
+++ ModeloSeguradora/Segurado.py
@@ -14,3 +14,29 @@ class Segurado:
         return (
             "Nome: {} - Data de Nascimento: {} - CPF: {} - RG: {}".format(self.nome_completo(), self._data_nascimento,
                                                                           self._cpf, self._rg))
+
+class Endereco:
+    def __init__(self, rua, numero, complemento, cep, estado, cidade, pais):
+        self._rua = rua
+        self.numero = numero
+        self._complemento = complemento
+        self._cep = cep
+        self._estado = estado
+        self._cidade = cidade
+        self.pais = pais
+
+    def endereco_completo(self):
+        return ({"Rua": self._rua, "Numero": self._numero, "Bairro": self._bairro, "Cidade": self._cidade,
+                 "Estado": self._estado, "Pais": self._pais})
+
+
+class Contato:
+    def __init__(self, celular, telefone_residencial, telefone_comercial, email):
+        self._celular = celular
+        self._telefone_residencial = telefone_residencial
+        self._telefone_comercial = telefone_comercial
+        self._email = email
+
+    def contato(self):
+        return ({"Celular": self._celular, "Telefone Residencial": self._telefone_residencial,
+                 "Telefone Comercial": self._telefone_comercial, "Email": self._email})
